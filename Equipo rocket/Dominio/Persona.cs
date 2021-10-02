c615b372b6e0296b338d4b2a16ecc8e0f28641ac
Alucard Belmont <manoloinsuasty@icloud.com>
2021-10-02 13:19:39 -0500

    post get front

--- Equipo rocket/Dominio/Persona.cs
+++ Equipo rocket/Dominio/Persona.cs
@@ -3,6 +3,7 @@ namespace Dominio
     public class Persona
     {
         public int Id {get; set; }
+        
         public string Nombre {get; set; }
         public int Edad {get; set; } 
         public int TipoDocumento {get; set; }
--- Equipo rocket/frontsito/Pages/Cliente/List.cshtml.cs
+++ Equipo rocket/frontsito/Pages/Cliente/List.cshtml.cs
@@ -11,9 +11,25 @@ namespace MyApp.Namespace
 {
     public class ListModel : PageModel
     {
+        private readonly IRepositorioCliente _repo;
         public IEnumerable<Cliente> Cliente { get; set;}
-        public void OnGet()
+        public ListModel(IRepositorioCliente repositorioCliente)
         {
+            _repo = repositorioCliente;
+        }
+
+        public DeleteModel(IRepositorioCliente repo)
+        {
+            _repo=repo;
+        }
+
+        public void OnGet(int id)
+        {
+            Cliente = _repo.GetCliente(id);
+        }
+        public void OnPost(int id)
+        {
+            _repo.DeleteCliente(id);
         }
     }
 }
